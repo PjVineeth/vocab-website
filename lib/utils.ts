e050489ec9b8e54eca4f7632791d71305081e539
Pj Vineeth Kumar <pjkvm123@gmail.com>
2025-06-06 15:49:19 +0530

    Restored the 15th Commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 // helper function to merge classes
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
