854b39d1fdd65d4d6f0378341a22b7296bc8604d
Pj Vineeth Kumar <pjkvm123@gmail.com>
2025-06-07 20:55:46 +0530

    Upadted Sarathi URL

--- app/page.tsx
+++ app/page.tsx
@@ -212,7 +212,7 @@ export default function Home() {
                       className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50"
                     >
                       <li>
-                        <a href="http://27.111.72.61:900" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">SARATHI-AI</a>
+                        <a href="http://27.111.72.61:9002" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">SARATHI-AI</a>
                       </li>
                       <li>
                         <a href="#chitra-ai" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">CHITRA-AI</a>
